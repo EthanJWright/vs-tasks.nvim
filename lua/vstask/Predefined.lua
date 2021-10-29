82b3f609a876cc5f09fe39170ba78bb27c047e8c
EthanJWright <ewright362@gmail.com>
2021-10-28 20:02:55 -0600

    feat: adding implementations for all predefined variables (copilot WTF)

--- lua/vstask/Predefined.lua
+++ lua/vstask/Predefined.lua
@@ -9,20 +9,87 @@ function Split(s, delimiter)
   return result;
 end
 
+-- get the path seperator for the current os
+local get_path_seperator = function()
+  return vim.fn.pathsep()
+end
+
 -- get filename from path string
 -- returns the filename from a absolute path
 -- @param path character to split on
 local get_filename = function(path)
-  local split = Split(path, "/")
+  local sep = get_path_seperator()
+  local split = Split(path, sep)
   return split[#split]
 end
 
 local get_relative_file_dirname = function()
+  local sep = get_path_seperator()
   local path = vim.fn.expand("%:p:h") -- get the path
-  local split = Split(path, "/") -- split on /
+  local split = Split(path, sep) -- split on /
   table.remove(split, #split) -- remove filename
-  return table.concat(split, "/") -- join back together
+  return table.concat(split, sep) -- join back together
 end
+
+-- get the current opened files base name (without extension)
+local get_current_file_basename_no_extension = function()
+  local sep = get_path_seperator()
+  local path = vim.fn.expand("%:p:h") -- get the path
+  local split = Split(path, sep) -- split on /
+  local filename = split[#split] -- get the filename
+  split = Split(filename, ".") -- split on .
+  table.remove(split, #split) -- remove extension
+  return table.concat(split, ".") -- join back together
+end
+
+-- get the current opened files base name
+local get_current_file_basename = function()
+  local sep = get_path_seperator()
+  local path = vim.fn.expand("%:p:h") -- get the path
+  local split = Split(path, sep) -- split on /
+  local filename = split[#split] -- get the filename
+  return filename
+end
+
+-- get current opened files dirname
+local get_current_file_dirname = function()
+  local sep = get_path_seperator()
+  local path = vim.fn.expand("%:p:h") -- get the path
+  local split = Split(path, sep) -- split on /
+  table.remove(split, #split) -- remove filename
+  return table.concat(split, sep) -- join back together
+end
+
+-- get the current open files extension
+local get_current_file_extension = function()
+  local sep = get_path_seperator()
+  local path = vim.fn.expand("%:p:h") -- get the path
+  local split = Split(path, sep) -- split on /
+  local filename = split[#split] -- get the filename
+  split = Split(filename, ".") -- split on .
+  return split[#split] -- get the extension
+end
+
+-- get the current working directory
+local get_current_dir = function()
+  return vim.fn.fnamemodify(vim.fn.getcwd(), ":h")
+end
+
+-- get the current line number
+local get_current_line_number = function()
+  return vim.fn.line(".")
+end
+
+-- get the selected text
+local get_selected_text = function()
+  return vim.fn.getreg("*")
+end
+
+-- get the exec path
+local get_exec_path = function()
+  return vim.fn.executable()
+end
+
 
 local get_relative_file = function()
   return vim.fn.bufname()
@@ -30,6 +97,16 @@ end
 
 local get_file = function()
   return get_filename(vim.fn.bufname())
+end
+
+-- get the file workspace folder
+local get_file_workspace_folder = function()
+  local sep = get_path_seperator()
+  local path = vim.fn.expand("%:p:h") -- get the path
+  local split = Split(path, sep) -- split on /
+  table.remove(split, #split) -- remove filename
+  table.remove(split, #split) -- remove filename
+  return table.concat(split, sep) -- join back together
 end
 
 local get_workspacefolder_basename = function()
@@ -40,17 +117,17 @@ return {
   [ "workspaceFolder" ] = vim.fn.cwd,
   [ "workspaceFolderBasename" ] = get_workspacefolder_basename,
   [ "file" ] = get_file,
-  [ "fileWorkspaceFolder" ] = nil,
+  [ "fileWorkspaceFolder" ] = get_file_workspace_folder,
   [ "relativeFile" ] = get_relative_file,
   [ "relativeFileDirname" ] = get_relative_file_dirname,
-  [ "fileBasename" ] = nil,
-  [ "fileBasenameNoExtension" ] = nil,
-  [ "fileDirname" ] = nil,
-  [ "fileExtname" ] = nil,
-  [ "cwd" ] = nil,
-  [ "lineNumber" ] = nil,
-  [ "selectedText" ] = nil,
-  [ "execPath" ] = nil,
+  [ "fileBasename" ] = get_current_file_basename,
+  [ "fileBasenameNoExtension" ] = get_current_file_basename_no_extension,
+  [ "fileDirname" ] = get_current_file_dirname,
+  [ "fileExtname" ] = get_current_file_extension,
+  [ "cwd" ] = get_current_dir,
+  [ "lineNumber" ] = get_current_line_number,
+  [ "selectedText" ] = get_selected_text,
+  [ "execPath" ] = get_exec_path,
   [ "defaultBuildTask" ] = nil,
   [ "pathSeparator" ] = nil
 }
